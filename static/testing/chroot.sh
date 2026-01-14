43cdc0438aac42763ee39901f89a49a09c806803
ewof <marroq@marroq.xyz>
2026-01-14 04:15:46 -0500

    port these from old repo

---
+++ static/testing/chroot.sh
@@ -0,0 +1,32 @@
+#Potential variables: timezone, lang and local
+
+ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
+
+hwclock --systohc
+
+timedatectl set-timezone "America/New_York"
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+mkinitcpio -P
+
+passwd << EOF
+123
+123
+EOF
+
+pacman --noconfirm --needed -S networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/current
+ln -s /etc/runit/sv/ntpd /etc/runit/runsvdir/current
+
+pacman --noconfirm --needed -S grub efibootmgr && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg
+
+pacman --noconfirm --needed -S dialog git
+larbs() { curl -O https://raw.githubusercontent.com/ewof/LARBS/custom/static/larbs.sh && bash larbs.sh;}
+dialog --title "Install elwolf's Rice" --yesno "This install script will easily let you access elwolf's Auto-Rice Boostrapping Scripts which automatically install a full rice.\n\nIf you'd like to install this, select yes, otherwise select no.\n\elwolf"  15 60 && larbs
